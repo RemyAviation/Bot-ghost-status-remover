2ff5459acda1f663879d01d15f610987fbfbf3c8
RemyAviation <123282443+RemyAviation@users.noreply.github.com>
2024-01-16 23:13:39 -0600

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["👑 I am RTX","Spotify","Youtube","SUBSCRIBE❤️","THANKS 🌇"];
+const statusMessages = ["TT: remy_aviation","IG: remy_aviation"];
 
 
 let currentIndex = 0;
